cff7cec7b85dc570b150cc22d6f610e7a0050126
haddadanthony <haddad-anthony@live.com>
2020-09-06 20:25:52 +0300

    Added tailwind

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends("layouts.layout")
+
+@section("content")
+    <p class="text-2xl uppercase">Hello world</p>
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Anthony Haddad</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    {{-- Styles --}}
+    <link rel="stylesheet" href="{{ asset("css/app.css") }}">
+
+</head>
+
+<body>
+    <div class="flex-center position-ref full-height">
+        
+
+        @yield("content")
+
+    </div>
+</body>
+
+</html>
