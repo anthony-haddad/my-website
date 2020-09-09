c6848410c90bbbb06b7e242e9e351e509f4d65eb
haddadanthony <haddad-anthony@live.com>
2020-09-09 20:53:04 +0300

    Create Header BladeComponent UI

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,11 +2,21 @@
     <nav class="flex items-center justify-between px-2 border-b-8 border-solid border-tropical">
         <h2 class="text-2xl font-semibold font-montserrat-alternates text-white cursor-pointer">Anthony Haddad</h2>
         <div class="flex justify-around text-white">
-            <a class="py-4 px-8 hover:bg-tropical" href="#">
-                <i class="uil uil-home-alt text-3xl cursor-pointer"></i>
+            <a class="group py-2 px-8 hover:bg-tropical flex flex-col items-center" href="#">
+                <i class="uil uil-home-alt transition duration-200 ease-in text-3xl cursor-pointer transform translate-y-2 transform group-hover:-translate-y-1"></i>
+                <p class="text-xs transition duration-200 ease-in-out opacity-0 text-white group-hover:opacity-100 transform group-hover:translate-y-1">Home</p>
             </a>
-            <a class="py-4 px-8 hover:bg-tropical" href="#">
-                <i class="uil uil-envelope text-3xl cursor-pointer"></i>
+            <a class="group py-2 px-8 hover:bg-tropical flex flex-col items-center" href="#">
+                <i class="uil uil-user transition duration-200 ease-in text-3xl cursor-pointer transform translate-y-2 transform group-hover:-translate-y-1"></i>
+                <p class="text-xs transition duration-200 ease-in-out opacity-0 text-white group-hover:opacity-100 transform group-hover:translate-y-1">Bio</p>
+            </a>
+            <a class="group py-2 px-8 hover:bg-tropical flex flex-col items-center" href="#">
+                <i class="uil uil-book-open transition duration-200 ease-in text-3xl cursor-pointer transform translate-y-2 transform group-hover:-translate-y-1"></i>
+                <p class="text-xs transition duration-200 ease-in-out opacity-0 text-white group-hover:opacity-100 transform group-hover:translate-y-1">Blog</p>
+            </a>
+            <a class="group py-2 px-8 hover:bg-tropical flex flex-col items-center" href="#">
+                <i class="uil uil-envelope transition duration-200 ease-in text-3xl cursor-pointer transform translate-y-2 transform group-hover:-translate-y-1"></i>
+                <p class="text-xs transition duration-200 ease-in-out opacity-0 text-white group-hover:opacity-100 transform group-hover:translate-y-1">Contact</p>
             </a>
         </div>
     </nav>
